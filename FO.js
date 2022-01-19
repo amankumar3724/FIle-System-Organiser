e53a2045b0b544d0360525fab68614f70a070508
amankumar3724 <amankumar3724@gmail.com>
2022-01-19 22:35:37 +0530

    ..

--- FO.js
+++ FO.js
@@ -57,7 +57,8 @@ function helpfn() {
   console.log(`List of all the Commands-
                     1) Tree Command - node FO.js tree <dirname>
                     2) Organize Command- node FO.js organize <dirname>
-                    3) Help Command - node FO.js help`);
+                    3) Help Command - node FO.js help
+                          Thank You`);
 }
 
 function organizeFn(dirpath) {
